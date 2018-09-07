e43b8d68a97b8e7d826316e3bbf92d21c45db53e
diana.ionita <diana.ionita@lifeworks.com>
2018-09-07 08:22:31 +0100

    trying to figure out how to set the cache settings per method

--- src/apiGatewayCachingPlugin.js
+++ src/apiGatewayCachingPlugin.js
@@ -1,21 +1,87 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+
 // TODO async
-const getRestApiId = (serverless) => {
+const getRestApiId = async serverless => {
   const stackName = serverless.providers.aws.naming.getStackName(serverless.service.provider.stage);
-  return serverless.providers.aws.request('CloudFormation', 'describeStacks', { StackName: stackName },
+
+  let stack = await serverless.providers.aws.request('CloudFormation', 'describeStacks', { StackName: stackName },
     serverless.service.provider.stage,
     serverless.service.provider.region
-  ).then((result) => {
-    return result.Stacks[0].Outputs
-      .filter(output => output.OutputKey === 'MyRestApiId')
-      .map(output => output.OutputValue)[0];
-  });
+  );
+
+  return stack.Stacks[0].Outputs
+    .filter(output => output.OutputKey === 'MyRestApiId')
+    .map(output => output.OutputValue)[0];
 }
 
 const configureApiGatewayCaching = async serverless => {
-  let id = await getRestApiId(serverless);
-  serverless.cli.log(`# After Deploy # Rest Api Id: ${id}`);
+  //let restApiId = await getRestApiId(serverless);
+  let restApiId = 'hg714gr6wl';
+
+  AWS.config.update({
+    region: serverless.service.provider.region,
+  });
+
+  const apiGateway = new AWS.APIGateway();
+  let params = {
+    restApiId,
+    stageName: 'devdiana',
+    patchOperations: [
+      {
+        op: 'replace',
+        path: '/cacheClusterEnabled',
+        value: 'true'
+      },
+      {
+        op: 'replace',
+        path: '/cacheClusterSize',
+        value: '0.5'
+      },
+      {
+        op: 'replace',
+        path: '/cats/GET/caching/enabled',
+        value: 'true'
+      },
+      {
+        op: 'replace',
+        path: '/cats/GET/caching/ttlInSeconds',
+        value: '10'
+      },
+      {
+        op: 'replace',
+        path: '/~1cats~1\{pawId\}/GET/caching/enabled',
+        value: 'true'
+      },
+      {
+        op: 'replace',
+        path: '/~1cats~1\{pawId\}/GET/caching/ttlInSeconds',
+        value: '15'
+      }
+    ]
+  }
+  let result = await apiGateway.updateStage(params).promise();
+  serverless.cli.log(`# Update Stage result: ${JSON.stringify(result)}`);
+  console.log('Okay so far');
+
+  // let params2 = {
+  //   httpMethod: 'GET',
+  //   restApiId,
+  //   resourceId: '6b98bx',
+  //   patchOperations: [
+  //     {
+  //       op: 'replace',
+  //       path: '/~1cats~1\{pawId\}/GET/requestParameters/method.request.path.pawId',
+  //       value: 'true'
+  //       //value: '\{\"method.request.path.pawId\": \"true\",\"method.request.header.Accept-Language\": \"true\"\}'
+  //     }
+  //   ]
+  // }
+
+  // let result2 = await apiGateway.updateIntegration(params2).promise();
+  // serverless.cli.log(`# Update Integration result: ${JSON.stringify(result2)}`);
+  console.log('Okay so far');
 }
 
 class ApiGatewayCachingPlugin {
@@ -25,7 +91,7 @@ class ApiGatewayCachingPlugin {
 
     this.hooks = {
       'before:package:finalize': this.beforeDeploy.bind(this),
-      'after:aws:deploy:finalize:cleanup': this.setApiGatewayCaching.bind(this),
+      // 'after:aws:deploy:finalize:cleanup': this.setApiGatewayCaching.bind(this),
     };
   }
 
@@ -34,21 +100,24 @@ class ApiGatewayCachingPlugin {
     let restApiId = {
       Ref: 'ApiGatewayRestApi',
     };
-
-    // Use the provider API gateway if one has been provided.
+    
+    // use the provided restApiId, if any
     if (this.serverless.service.provider.apiGateway && this.serverless.service.provider.apiGateway.restApiId) {
       restApiId = this.serverless.service.provider.apiGateway.restApiId
     }
-
+    
+    // TODO rename var
     this.serverless.service.provider.compiledCloudFormationTemplate.Outputs.MyRestApiId = {
       Description: 'Rest API Id',
       Value: restApiId,
     };
-  }
-
-  setApiGatewayCaching() {
+    
     return configureApiGatewayCaching(this.serverless);
   }
+
+  // setApiGatewayCaching() {
+  //   return configureApiGatewayCaching(this.serverless);
+  // }
 }
 
 module.exports = ApiGatewayCachingPlugin;
