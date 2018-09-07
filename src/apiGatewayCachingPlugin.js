17d732e82f6fbd6eebba9756b233435fe85c1808
diana.ionita <diana.ionita@lifeworks.com>
2018-09-07 13:53:51 +0100

    got it working, adding model

--- src/apiGatewayCachingPlugin.js
+++ src/apiGatewayCachingPlugin.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const ApiGatewayCachingSettings = require('./apiGatewayCachingSettings');
 
-// TODO async
 const getRestApiId = async serverless => {
   const stackName = serverless.providers.aws.naming.getStackName(serverless.service.provider.stage);
 
@@ -65,23 +65,67 @@ const configureApiGatewayCaching = async serverless => {
   serverless.cli.log(`# Update Stage result: ${JSON.stringify(result)}`);
   console.log('Okay so far');
 
+  // let template = {
+  //   Resources: {
+  //     ApiGatewayMethodCatsPawidVarGet: serverless.service.provider.compiledCloudFormationTemplate.Resources.ApiGatewayMethodCatsPawidVarGet
+  //   }
+  // };
+
+  let method = serverless.service.provider.compiledCloudFormationTemplate.Resources.ApiGatewayMethodCatsPawidVarGet;
+  method.Properties.RequestParameters = {
+    "method.request.path.pawId": true,
+    "method.request.header.Accept-Language": false
+  };
+  method.Properties.Integration.RequestParameters = {
+    "integration.request.path.pawId": "method.request.path.pawId",
+    "integration.request.header.Accept-Language": "method.request.header.Accept-Language"
+  };
+  method.Properties.Integration.CacheNamespace = "ApiGatewayMethodCatsPawidVarGetCacheNS";
+  method.Properties.Integration.CacheKeyParameters = ["method.request.path.pawId", "method.request.header.Accept-Language"];
+
   // let params2 = {
   //   httpMethod: 'GET',
   //   restApiId,
-  //   resourceId: '6b98bx',
+  //   resourceId: '6b98bx', // TODO
   //   patchOperations: [
+  //     // I tried this but then settled for referencing them in the serverless.yml under events/http/request/parameters/...
+  //     // {
+  //     //   op: 'replace',
+  //     //   path: '/~1cats~1\{pawId\}/GET/requestParameters',
+  //     //   value: '\{\"method.request.path.pawId\": \"true\",\"method.request.header.Accept-Language\": \"true\"\}'
+  //     // },
+  //     // {
+  //     //   op: 'replace',
+  //     //   path: '/~1cats~1/*/GET/requestParameters/method.request.path.pawId/caching/enabled',
+  //     //   value: 'true'
+  //     // },
   //     {
   //       op: 'replace',
-  //       path: '/~1cats~1\{pawId\}/GET/requestParameters/method.request.path.pawId',
+  //       path: '/requestParameters',
   //       value: 'true'
-  //       //value: '\{\"method.request.path.pawId\": \"true\",\"method.request.header.Accept-Language\": \"true\"\}'
-  //     }
+  //     },
   //   ]
   // }
 
   // let result2 = await apiGateway.updateIntegration(params2).promise();
   // serverless.cli.log(`# Update Integration result: ${JSON.stringify(result2)}`);
-  console.log('Okay so far');
+  // console.log('Okay so far');
+
+  // const templateBody = fs.readFileSync(`./node_modules/serverless-api-gateway-caching/src/cf.yml`, 'utf8');
+  // let params2 = {
+  //   StackName: `cat-api-devdiana`,
+  //   TemplateBody: JSON.stringify(template)
+  // }
+
+  // const cloudformation = new AWS.CloudFormation();
+  // let cfResponse = await cloudformation.updateStack(params2).promise();
+  // console.log('Okay so far');
+  // console.log(`Updating CloudFormation stack with Id ${cfResponse.StackId}...`);
+  // let waitReq = {
+  //   StackName: cfResponse.StackId
+  // };
+  // await cloudformation.waitFor('stackUpdateComplete', waitReq).promise();
+  // console.log(`Done updating CloudFormation stack with Id ${cfResponse.StackId}`);
 }
 
 class ApiGatewayCachingPlugin {
@@ -90,28 +134,35 @@ class ApiGatewayCachingPlugin {
     this.options = options;
 
     this.hooks = {
-      'before:package:finalize': this.beforeDeploy.bind(this),
+      'before:package:initialize': this.beforePackageInitialize.bind(this),
+      // 'before:package:finalize': this.beforeDeploy.bind(this),
       // 'after:aws:deploy:finalize:cleanup': this.setApiGatewayCaching.bind(this),
     };
   }
 
   // TODO rename
-  beforeDeploy() {
-    let restApiId = {
-      Ref: 'ApiGatewayRestApi',
-    };
-    
-    // use the provided restApiId, if any
-    if (this.serverless.service.provider.apiGateway && this.serverless.service.provider.apiGateway.restApiId) {
-      restApiId = this.serverless.service.provider.apiGateway.restApiId
+  beforePackageInitialize() {
+    this.settings = new ApiGatewayCachingSettings(this.serverless);
+    // let restApiId = {
+    //   Ref: 'ApiGatewayRestApi',
+    // };
+
+    // // use the provided restApiId, if any
+    // if (this.serverless.service.provider.apiGateway && this.serverless.service.provider.apiGateway.restApiId) {
+    //   restApiId = this.serverless.service.provider.apiGateway.restApiId
+    // }
+
+    // // TODO rename var
+    // this.serverless.service.provider.compiledCloudFormationTemplate.Outputs.MyRestApiId = {
+    //   Description: 'Rest API Id',
+    //   Value: restApiId,
+    // };
+  }
+
+  beforePackageFinalize() {
+    if (!this.settings.cachingEnabled) {
+      return;
     }
-    
-    // TODO rename var
-    this.serverless.service.provider.compiledCloudFormationTemplate.Outputs.MyRestApiId = {
-      Description: 'Rest API Id',
-      Value: restApiId,
-    };
-    
     return configureApiGatewayCaching(this.serverless);
   }
 
---
+++ src/apiGatewayCachingSettings.js
@@ -0,0 +1,43 @@
+class ApiGatewayEndpointCachingSettings {
+  constructor(functionName, functionSettings) {
+    this.functionName = functionName;
+
+    // TODO multiple http endpoints
+    let cachingConfig = functionSettings.events.filter(e => e.http != null)[0].http.caching;
+    if (!cachingConfig) {
+      this.cachingEnabled = false;
+    }
+    this.cachingEnabled = cachingConfig.enabled;
+    this.cacheTtlInSeconds = cachingConfig.ttlInSeconds;
+    this.cacheKeyParameters = cachingConfig.cacheConfigParameters;
+  }
+}
+
+class ApiGatewayCachingSettings {
+  constructor(serverless) {
+    if (!serverless.service.custom.apiGatewayCaching) {
+      this.cachingEnabled = false;
+      return;
+    }
+    this.cachingEnabled = serverless.service.custom.apiGatewayCaching.enabled;
+    this.cachingClusterSize = serverless.service.custom.apiGatewayCaching.clusterSize;
+    this.cacheTtlInSeconds = serverless.service.custom.apiGatewayCaching.ttlInSeconds;
+
+    this.endpointSettings = [];
+    for (let functionName in serverless.service.functions) {
+      let functionSettings = serverless.service.functions[functionName];
+      if (this.isApiGatewayEndpoint(functionSettings)) {
+        this.endpointSettings.push(new ApiGatewayEndpointCachingSettings(functionName, functionSettings))
+      }
+    }
+  }
+
+  isApiGatewayEndpoint(functionSettings) {
+    // TODO isEmpty
+    if (!functionSettings.events) {
+      return false;
+    }
+    return functionSettings.events.filter(e => e.http != null).length > 0;
+  }
+}
+module.exports = ApiGatewayCachingSettings
