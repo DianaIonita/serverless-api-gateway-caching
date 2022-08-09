2508d5733e6a7613509470447c8f13d2af31fe88
Jeremiah Langner <jlangner@black-bile.local>
2022-08-09 09:22:31 -0700

    add sls patch debug log for caching

--- src/stageCache.js
+++ src/stageCache.js
@@ -168,7 +168,8 @@ const updateStageFor = async (serverless, params, stage, region) => {
   else {
     paramsInChunks.push(params);
   }
-
+  
+  serverless.cli.log(paramsInChunks);
   for (let index in paramsInChunks) {
     serverless.cli.log(`[serverless-api-gateway-caching] Updating API Gateway cache settings (${parseInt(index) + 1} of ${paramsInChunks.length}).`);
     await serverless.providers.aws.request('APIGateway', 'updateStage', paramsInChunks[index], stage, region);
